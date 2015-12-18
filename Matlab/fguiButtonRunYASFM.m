31f2c3b8ca7dcf971a7e9d12887f7bbbfd07d367
Filip Srajer <filip@srajer.eu>
2015-12-18 17:35:23 +0100

    mkdir when ran from matlab.

--- Matlab/fguiButtonRunYASFM.m
+++ Matlab/fguiButtonRunYASFM.m
@@ -2,6 +2,10 @@ function fs = fguiButtonRunYASFM(fs, fn)
 
 dataDir = fullfile(fs.DataDirectory,fs.DatasetSubdirectory);
 
+if ~exist(dataDir,'dir')
+    mkdir(dataDir);
+end
+
 command = [fs.YASFMExe ' ' dataDir ' ' fs.imgsSubdir ' ' ...
     num2str(fs.firstOctave) ' ' fs.ccdWidthDb];
 
