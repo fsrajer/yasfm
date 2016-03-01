f13e4269a9ffc94ed23f165baf5f0544e1919939
Filip Srajer <filip@srajer.eu>
2016-03-01 16:58:05 +0100

    Find results for precision-recall curves automatically by approach name.

--- Matlab/GeometricVerification/evaluateMatches.m
+++ Matlab/GeometricVerification/evaluateMatches.m
@@ -1,18 +1,28 @@
 %% setup
 addpath ..
 
+% to be set by the user
 dataDir = 'C:\Users\Filip\Workspace\Data\pairs';
 allFn = 'tentatively_matched_all.txt';
 gtFn = [allFn(1:end-4) '_ground_truth.mat'];
-baseFn = 'matched_ratio';
-nFns = 5;
+names = {'ratio','ratio'};
 
 allFn = fullfile(dataDir,allFn);
 gtFn = fullfile(dataDir,gtFn);
-fns = cell(1,nFns);
-for i=1:nFns
-    fns{i} = fullfile(dataDir,[baseFn num2str(i) '.txt']);
+nNames = numel(names);
+fns = cell(1,nNames);
+matlabDir = pwd;
+for i=1:nNames
+    d = fullfile(dataDir,names{i});
+    cd(d);
+    fns{i} = cellstr(ls('matched*'));
+    
+    % sort based on numbers
+    r = regexp(fns{i},'\d+','match'); %find numbers
+    [~, ind] = sort(cellfun(@(c) str2double(c{1}), r)); % convert to numbers and sort
+    fns{i} = fns{i}(ind); %// use index to build result
 end
+cd(matlabDir);
 
 %% read data
 ignoreFeats = true;
@@ -23,10 +33,13 @@ nImgs = numel(data.cams);
 ld = load(gtFn);
 labels = ld.labels;
 
-filteredPairs = cell(1,nFns);
-for i=1:nFns
-    data = readResults(fns{i},ignoreFeats);
-    filteredPairs{i} = data.pairs;
+filteredPairs = cell(1,nNames);
+for i=1:nNames
+    filteredPairs{i} = cell(1,numel(fns{i}));
+    for j=1:numel(fns{i})
+        data = readResults(fullfile(dataDir,names{i},fns{i}{j}),ignoreFeats);
+        filteredPairs{i}{j} = data.pairs;
+    end
 end
 
 %% show curves
@@ -41,19 +54,24 @@ for i=1:nImgs
         
         if n>0
             all = allPairs(i,j).matches(:,toUse);
-            precision = zeros(1,nFns);
-            recall = zeros(1,nFns);
-            for k=1:nFns
-                curr = filteredPairs{k}(i,j).matches;
-                est = ismember(all',curr','rows')';
-                truePositive = sum(gt==1 & est==1);
-                falsePositive = sum(gt==0 & est==1);
-                falseNegative = sum(gt==1 & est==0);
-                precision(k) = truePositive/(truePositive+falsePositive);
-                recall(k) = truePositive/(truePositive+falseNegative);
-            end
             figure;
-            plot(recall,precision,'bx-','linewidth',1.5);
+            hold on
+            for in=1:nNames
+                nFns = numel(fns{i});
+                precision = zeros(1,nFns);
+                recall = zeros(1,nFns);
+                for k=1:nFns
+                    curr = filteredPairs{i}{k}(i,j).matches;
+                    est = ismember(all',curr','rows')';
+                    truePositive = sum(gt==1 & est==1);
+                    falsePositive = sum(gt==0 & est==1);
+                    falseNegative = sum(gt==1 & est==0);
+                    precision(k) = truePositive/(truePositive+falsePositive);
+                    recall(k) = truePositive/(truePositive+falseNegative);
+                end
+                plot(recall,precision,'x-','linewidth',1.5);
+            end
+            legend(names);
             title('precision-recall')
             xlabel('recall');
             ylabel('precision');
