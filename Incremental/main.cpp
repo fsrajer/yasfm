f38ec9d05535554e77877cffa521f48f3427c4f8
Filip Srajer <filip@srajer.eu>
2016-04-16 16:37:58 +0200

    [minor] main.cpp updates.

--- Incremental/main.cpp
+++ Incremental/main.cpp
@@ -260,9 +260,9 @@ int main(int argc,const char* argv[])
   data.clearDescriptors();
 
   data.writeASCII("tentatively_matched_all.txt");*/
-  data.readASCII("tentatively_matched_all.txt");
-  //data.readASCII("tentatively_matched_all_with_gt.txt");
-  
+  //data.readASCII("0.8/tentatively_matched_all.txt");
+  data.readASCII("0.8/tentatively_matched_all_with_gt.txt");
+
   for(int i = 0; i < data.numCams(); i++)
   {
     auto& cam = data.cam(i);
@@ -273,9 +273,11 @@ int main(int argc,const char* argv[])
   bool useCalibratedEpipolarVerif = true;
 
   float& ratioThresh = opt.getOpt<OptionsFLANN>("matchingFLANN").get<float>("ratioThresh");
-  ratioThresh = 0.6f;
-  int nSteps = 3;
-  string name = "H-EG-greedy";
+  ratioThresh = 0.f;
+  //int nSteps = 1;
+  int nSteps = 33;
+  float stepSize = 0.025f;
+  string name = "0.8/gv-fast";
   _mkdir(joinPaths(dir,name).c_str());
   pair_umap<CameraPair> allPairs = data.pairs();
   
@@ -284,7 +286,7 @@ int main(int argc,const char* argv[])
   allPairs[curr] = data.pairs()[curr];
   data.pairs() = allPairs;*/
   
-  for(int i = 0; i < nSteps; i++,ratioThresh += 0.1f)
+  for(int i = 0; i < nSteps; i++,ratioThresh += stepSize)
   {
     cout << "Processing " << i << "-th out of " << nSteps << "\n";
     for(auto& entry : data.pairs())
