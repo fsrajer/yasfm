1e45e79a44ee3e7704ba702bbcbcb593f9c688ab
Filip Srajer <filip@srajer.eu>
2016-05-10 11:16:57 +0200

    Plot scaled pr curves of different methods.

--- Matlab/GeometricVerification/plotPrecisionRecall.m
+++ Matlab/GeometricVerification/plotPrecisionRecall.m
@@ -1,10 +1,13 @@
-function h = plotPrecisionRecall(pr,rc,col)
-
-h = plot(rc,pr,'x-','linewidth',1.5,'color',col);
+function h = plotPrecisionRecall(pr,rc,col,scale)
+if nargin < 4
+    scale = 1;
+end
+h = plot(rc,pr,'x-','linewidth',1.5*scale,'color',col);
 if(numel(pr)>=33)
     plot(rc([25 29 33]),pr([25 29 33]),...
-        'x','markersize',20,'linewidth',5,'color',col);
+        'x','markersize',20*scale,'linewidth',5*scale,'color',col);
 end
+axis equal
 
 for k=1:numel(pr)
     text(rc(k),pr(k),num2str(k));
