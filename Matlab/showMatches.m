e269151b871a2a41c0bee24a7423dc10a222992f
Filip Srajer <filip@srajer.eu>
2016-05-15 23:00:04 +0200

    Refactoring showMatches script.

--- Matlab/showMatches.m
+++ Matlab/showMatches.m
@@ -56,7 +56,7 @@ for pair=pairsToShow
     h2 = size(imgs{j},1);
     offset = w1+ceil(separatorWidthScale*w1);
     
-    img = uint8(zeros(max(h1,h2),offset+w2,size(imgs{i},3)));
+    img = uint8(255+zeros(max(h1,h2),offset+w2,size(imgs{i},3)));
     img(1:h1,1:w1,:) = imgs{i};
     img(1:h2,(offset+1):(offset+w2),:) = imgs{j};
     
@@ -169,31 +169,32 @@ for pair=pairsToShow
 %     set(gcf,'PaperPosition',[0 0 sz(2) sz(1)]);
 %     set(gcf,'PaperSize',[sz(2) sz(1)]);
 %     fn = 'dalib-pair-matches';
+%     fn = sprintf('pair-%i-%i-gv-fast',i,j);
 %     print(gcf,fullfile('C:\Users\Filip\Dropbox\Diplomka\cmpthesis\imgs',fn),'-dpdf');
 end
 % save('HsGT.mat','HsGT','cams','pairs');
 end
 
 function cols = initColors()
-cols=cell(2,9);
-% cols{1,1} = [0.5 0.5 0.5];
-% cols{2,1} = [0.5 0.5 0.5];
-cols{1,1} = [0 0 0];
-cols{2,1} = [0 0 0];
-cols{1,2} = [0 1 0];
-cols{2,2} = [0 0.5 0];
-cols{1,3} = [0 0 1];
-cols{2,3} = [0 0 0.75];
-cols{1,4} = [1 0 0];
-cols{2,4} = [0.5 0 0];
-cols{1,5} = [1 1 0];
-cols{2,5} = [0.5 0.5 0];
-cols{1,6} = [1 0 1];
-cols{2,6} = [0.5 0 0.5];
-cols{1,7} = [0 1 1];
-cols{2,7} = [0 0.5 0.5];
-cols{1,8} = 'k';
-cols{2,8} = 'w';
-cols{1,9} = 'm';
-cols{2,9} = 'm';
+cols={};
+% cols{1,end+1} = [0.5 0.5 0.5];
+% cols{2,end} = [0.5 0.5 0.5];
+% cols{1,end+1} = [0 0 0];
+% cols{2,end} = [0 0 0];
+cols{1,end+1} = [0 1 0];
+cols{2,end} = [0 0.5 0];
+cols{1,end+1} = [0 0 1];
+cols{2,end} = [0 0 0.75];
+cols{1,end+1} = [1 0 0];
+cols{2,end} = [0.5 0 0];
+cols{1,end+1} = [1 1 0];
+cols{2,end} = [0.5 0.5 0];
+cols{1,end+1} = [1 0 1];
+cols{2,end} = [0.5 0 0.5];
+cols{1,end+1} = [0 1 1];
+cols{2,end} = [0 0.5 0.5];
+cols{1,end+1} = 'k';
+cols{2,end} = 'w';
+cols{1,end+1} = 'm';
+cols{2,end} = 'm';
 end
